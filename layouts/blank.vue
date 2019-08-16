70576e7461cb5680f4b26e9ab70b29ad14189007
dobeco <736278325@qq.com>
2019-08-16 17:20:31 +0800

    register and login components

---
+++ layouts/blank.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout-blank">
+    <nuxt/>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "blank",
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
