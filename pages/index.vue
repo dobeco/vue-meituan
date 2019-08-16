bbf69f719453854c3eeba4555e7f2f89fcbbdb0e
dobeco <736278325@qq.com>
2019-08-16 16:16:54 +0800

    home page menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,41 @@
 <template>
-  <div class="container">
+  <div class="page-index">
+    <el-row>
+      <el-col :span="5">
+        <emenu/>
+      </el-col>
+      <el-col :span="19">
+        <life></life>
 
+      </el-col>
+    </el-row>
+    <el-row >
+      <el-col :span="24">
+        <artistic></artistic>
 
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-
+import Emenu from '@/components/index/menu.vue'
+import Life from '@/components/index/life.vue'
+import Artistic from '@/components/index/artistic.vue'
 
 export default {
+  name: 'index',
+
   components: {
+    Emenu,
+    Life,
+    Artistic
 
 
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-
-
-}
-
-
-
-.links {
-  padding-top: 15px;
-}
+<style lang="scss">
+  @import "@/assets/css/index/index.scss";
 </style>
