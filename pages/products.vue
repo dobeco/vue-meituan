68c5c0fbb0b53544ce9e75302314e92f240d1bcd
dobeco <736278325@qq.com>
2019-08-18 10:38:30 +0800

    products and map components

---
+++ pages/products.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="page-product"></div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'products',
+
+  data(){
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+  @import "@/assets/css/products/index.scss";
+</style>
